94a2a14bbcd184cf6c49d875b15c9f8f1b793102
Jonas Andersen <Jonas@anders1.com>
2024-12-17 15:29:07 +0100

    UPT:Testing if updated

--- src/Sections/Hero.tsx
+++ src/Sections/Hero.tsx
@@ -1,7 +1,14 @@
 const Hero = () => {
   return (
     <section id="hero">
-      <h3>Hero</h3>
+      <h3>JAbba dabbba doo</h3>
+      <p>
+        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Nemo magnam
+        officiis error, magni explicabo nam omnis quaerat ipsa quo impedit
+        fugiat eos quae eius possimus sapiente numquam! Animi ea libero quasi!
+        Voluptate maiores voluptatem nihil at dolore! Reprehenderit, nesciunt
+        deserunt.
+      </p>
     </section>
   );
 };
